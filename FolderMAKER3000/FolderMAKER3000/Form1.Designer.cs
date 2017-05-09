3679493c47fc7f948672bdc5fa636e6f20c42085
DESKTOP-VPDQO05\Harrison.Perry <harrison.v.perry@gmail.com>
2017-05-09 16:31:04 +0100

    Init

---
+++ FolderMAKER3000/FolderMAKER3000/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace FolderMAKER3000
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Run!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(106, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Set folder.txt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(250, 63);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Not Set!";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(106, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Set Path";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(15, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(247, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Not Set!";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 323);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FolderMaker3000";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FolderMAKER3000/FolderMAKER3000/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderMAKER3000
+{
+    public partial class Form1 : Form
+    {
+        public List<string> names = new List<string>();
+        
+        private bool _pathSet;
+        private string _path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+
+            if (result == DialogResult.OK) // Test result.
+            {
+
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    foreach (string s in lines)
+                    {
+                        names.Add(s);
+                    }
+
+                    label2.Text = openFileDialog1.SafeFileName;
+
+
+                }
+                catch (IOException)
+                {
+                }
+            }
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+            if(_pathSet)
+            {
+                int index = 0;
+                bool legacyflag = true;
+                foreach (string s in names)
+                {
+
+                    if (legacyflag && s.Equals("Proprietary and Confidential"))
+                    {
+                        Directory.CreateDirectory(_path + @"\0" + "1.5" + " " + s);
+                        File.WriteAllText(_path + @"\0" + "1.5" + " " + s + "\\keep.txt", "");
+                        legacyflag = false;
+                    }
+
+                    else
+                    {
+                        if (index < 10)
+                        {
+                            Directory.CreateDirectory(_path + @"\0" + index + " " + s);
+                            File.WriteAllText(_path + @"\0" + index + " " + s + "\\keep.txt", "");
+                        }
+                        else
+                        {
+                            Directory.CreateDirectory(_path + @"\" + index + " " + s);
+                            File.WriteAllText(_path + @"\" + index + " " + s + "\\keep.txt", "");
+                        }
+                        index++;
+                    }
+
+
+                }
+
+                _pathSet = false;
+            }
+            else
+            {
+                MessageBox.Show("Please set the path!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if(result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath))
+            {
+                label4.Text = folderBrowserDialog1.SelectedPath;
+                _pathSet = true;
+                _path = folderBrowserDialog1.SelectedPath;
+
+                Process.Start(_path);
+            }
+        }
+
+ 
+    }
+}
